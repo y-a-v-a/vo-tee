547c4d11959b31376967319c2892647a7b68afde
Vincent Bruijn <vebruijn@gmail.com>
2012-09-30 10:04:26 +0200

    styling of form

--- form.php
+++ form.php
@@ -16,7 +16,8 @@ $key = FormHandler::getKey();
 		<link rel="stylesheet" href="style.css" type="text/css" />
 	</head>
 	<body>
-		<h1>vo-tee submission form</h1>
+		<h1>Vo-tee submission form</h1>
+		<h2><a href="index.php">Go to overview</a></h2>
 		<?php if ($handled === true): ?>
 			<h2>Thanks for submitting the form!</h2>
 		<?php endif; ?>
@@ -25,14 +26,13 @@ $key = FormHandler::getKey();
 		<?php endif; ?>
 		<form method="post" enctype="multipart/form-data">
 			<input type="hidden" name="key" value="<?=$key?>" />
-			<input type="file" name="design" accept="image/jpeg,image/tiff,image/vnd.adobe.photoshop,application/pdf,application/postscript" /><br/>
-			<input type="text" name="name" placeholder="your name" /><br/>
-			<input type="text" name="url" placeholder="your website" /><br/>
-			<input type="text" name="email" placeholder="your email address" /><br/>
-			<input type="text" name="hometown" placeholder="your hometown" /><br/>
-			<input type="submit" value="Send design!" />
+			Your design: <input type="file" name="design" accept="image/jpeg,image/tiff,image/vnd.adobe.photoshop,application/pdf,application/postscript" /><br/>
+			<input type="text" name="name" placeholder="Your name" /><br/>
+			<input type="text" name="url" placeholder="Your website" /><br/>
+			<input type="text" name="email" placeholder="Your email address" /><br/>
+			<input type="text" name="hometown" placeholder="Your hometown" /><br/>
+			<input type="submit" value="Submit design!" />
 		</form>
-		<a href="index.php">Goto home</a>
 	</body>
 </html>
 <?php
